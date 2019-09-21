1a2d7ef9d20138feb011b3dedc5cde9bfbe9f31b
AmadoMiguel <amadodevia@gmail.com>
2019-09-21 16:44:20 -0500

    added users ordering with Comparator example

---
+++ Java/September-2019/Exercises/src/com/users_list/UsersInQueue.java
@@ -0,0 +1,27 @@
+package com.users_list;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class UsersInQueue {
+	public static void main(String[] args) {
+		List<User> users = new ArrayList<User>();
+		User user1 = new User(1, "John", "Roland", 30);
+		User user2 = new User(2, "Mary", "Korg", 20);
+		User user3 = new User(3, "Deivid", "Gibson", 15);
+		User user4 = new User(4, "Andrew", "Tama", 50);
+//		Add users in any order
+		users.add(user2);
+		users.add(user4);
+		users.add(user1);
+		users.add(user3);
+		System.out.println(users);
+//		Users will be ordered by age (that's how is defined in the overriden compare method in the
+//		UsersOrdering class which implements the Comparator interface)
+		PriorityQueue<User> usersQueue = new PriorityQueue<>(new UsersOrdering());
+		usersQueue.addAll(users);
+		System.out.println(usersQueue);
+	}
+	
+}
---
+++ Java/September-2019/Exercises/src/com/users_list/UsersOrdering.java
@@ -0,0 +1,14 @@
+package com.users_list;
+
+import java.util.Comparator;
+
+public class UsersOrdering implements Comparator<User> {
+	
+//	Order users by age in the collection that is passed an instance of this class to specify ordering
+//	of objects
+	@Override
+	public int compare(User arg0, User arg1) {
+		return arg0.getAge() - arg1.getAge();
+	}
+	
+}
