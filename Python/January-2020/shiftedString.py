5f29fbcad25f9ce524b4f1eb2fef873435b73e3b
AmadoMiguel <amadodevia@gmail.com>
2020-01-25 12:04:59 -0500

    added check if string is shifted exercise

--- Python/January-2020/shiftedString.py
+++ Python/January-2020/shiftedString.py
@@ -1,7 +1,7 @@
 # You are given two strings, A and B. Return whether A can be shifted some number of times to get B.
 # #
 # # Eg. A = abcde, B = cdeab should return true because A can be shifted 3 times to the right to get B. A = abc and
-# # B= acb should return false.
+# # B= acb should return false
 
 
 def shiftString(string):
