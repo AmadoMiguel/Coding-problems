080e3e1f16026db8d3ea4cd93583d935c5e6cbde
AmadoMiguel <amadodevia@gmail.com>
2020-03-19 09:18:09 -0500

    added pick max change exercise

---
+++ Python/March-2020/pickingChange.py
@@ -0,0 +1,30 @@
+# Given a 2d n x m matrix where each cell has a certain amount of change on the floor, your goal is to start from
+# the top left corner mat[0][0] and end in the bottom right corner mat[n - 1][m - 1] with the most amount of change.
+# You can only move either right or down.
+
+
+def pickMaxChange(mtx, mtxLen, mtxRowLen, currPos, currChange):
+    for pos in [
+        [currPos[0] + 1, currPos[1]], [currPos[0], currPos[1] + 1]
+    ]:
+        if 0 <= pos[0] < mtxLen and 0 <= pos[1] < mtxRowLen:
+            pickMaxChange(mtx, mtxLen, mtxRowLen, pos, currChange + mtx[pos[0]][pos[1]])
+        else:
+            print(currChange)
+            if pickMaxChange.maxChange is None or currChange > pickMaxChange.maxChange:
+                pickMaxChange.maxChange = currChange
+
+
+def getMaxChange(matrix):
+    pickMaxChange.maxChange = None
+    pickMaxChange(matrix, len(matrix), len(matrix[0]), [0, 0], 0)
+    return pickMaxChange.maxChange
+
+
+mat = [
+    [0, 3, 0, 2],
+    [1, 2, 3, 3],
+    [6, 0, 3, 2]
+]
+
+getMaxChange(mat)
